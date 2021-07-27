19060a7352c14c0b5ff4e67073de1db6cee61215
Oleg Simonov <olegsimonoff@mail.ru>
2021-07-27 13:24:02 +0600

    QuickFix #3

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def ssm_connection():
 @app.route("/update_server/", methods=['GET', 'POST'])
 def git_webhook():
     if request.method == 'POST':
-        repo = git.Repo('D:/Media/Documents/GitHub/ssm-backend')
+        repo = git.Repo('D:/Media/Documents/GitHub/ssm-backend') # testing this
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
