d1ab1a5a2074c3129162f687af017ee4c3c89990
WolfiD007 <dama.0@mail.ru>
2021-09-07 15:14:21 +0600

    Config with bot initialization

--- Salom_Bot.py
+++ Salom_Bot.py
@@ -1,17 +1,20 @@
 from flask import Flask, request, Blueprint
 import requests
+import telebot
+from config import token
 
 salom_bot = Blueprint("salom_bot", __name__)
 
+bot = telebot.TeleBot(token)
+
 def send_message(chat_id, text):
     method = "sendMessage"
-    token = "1994938654:AAHFLtVLwkog_4HK75-xTo8_-PA4vi4reuU"
     url = f"https://api.telegram.org/bot{token}/{method}"
     data = {"chat_id": chat_id, "text": text}
     requests.post(url, data=data)
 
 
-@salom_bot.route("/1994938654:AAHFLtVLwkog_4HK75-xTo8_-PA4vi4reuU/", methods=["POST"])
+@salom_bot.route("/" + token + "/", methods=["POST"])
 def receive_update():
     chat_id = request.json["message"]["chat"]["id"]
     send_message(chat_id, "Hello!")
