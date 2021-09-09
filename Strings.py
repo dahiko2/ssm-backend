102d19bd3f19ae1171ba577f66314614afded85a
WolfiD007 <dama.0@mail.ru>
2021-09-09 11:18:03 +0600

    Test

---
+++ Strings.py
@@ -0,0 +1,9 @@
+maktab_desc = "Maktab\n"\
+                 "\n"\
+                 "Serial haqida: MAKTAB.\n" \
+                 "O'quvchilarining hayoti qiziqarli voqealar! Ishkal,sevgi,do'stlik  shu va shunga o'xshash qissalarni har bir maktab o'quvchisi boshidan o'tqazgan!\n" \
+                 "\n" \
+                 "Hullas kalom biz sizlar uchun ajoyib hayotiy serial su'ratga oldik. Tomosha qilmasangiz kundalikga 2 tushadi aytib qo'ydim lekin;)\n" \
+                 "\n" \
+                 "AJOYIB JAMOA BILAN AJOYIB SERIAL"
+
