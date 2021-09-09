282961835bc785ffac68d1ff4cd90db7d58b5e6d
WolfiD007 <dama.0@mail.ru>
2021-09-09 16:16:04 +0600

    Update Salom_Bot.py

--- Salom_Bot.py
+++ Salom_Bot.py
@@ -128,5 +128,5 @@ def query_handler(call):
     elif call.data == '3':
         answer = 'Вы отличник!'
 
-    bot.send_message(call.message.chat.id, answer)
+    bot.send_message(call.message.chat.id, answer, reply_markup=start_markup)
 
