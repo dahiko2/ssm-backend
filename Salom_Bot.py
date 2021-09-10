82810d96dc8bc3dd2f4efb48f58bde8057df0504
WolfiD007 <dama.0@mail.ru>
2021-09-10 12:05:02 +0600

    Update Salom_Bot.py

--- Salom_Bot.py
+++ Salom_Bot.py
@@ -65,15 +65,14 @@ def start_message(message):
     count = 0
 
     status = ['creator', 'administrator', 'member']
-    for i in status:
-        if i == bot.get_chat_member(chat_id=-1001584831368, user_id=message.from_user.id).status:
-            chat_id = message.chat.id
-            bot.send_message(chat_id, "Доступ получен✅")
-            break
 
-        else:
-            chat_id = message.chat.id
-            bot.send_message(chat_id, text="Нету подписки")
+    if bot.get_chat_member(chat_id=-1001584831368, user_id=message.from_user.id).status in status:
+        chat_id = message.chat.id
+        bot.send_message(chat_id, "Доступ получен✅")
+
+    else:
+        chat_id = message.chat.id
+        bot.send_message(chat_id, text="Нету подписки")
 
     '''if is_subscribed("testtestert", "self") == False:
         bot.send_message(message.chat.id, "@salomserial kanaliga obuna bo'ling")
