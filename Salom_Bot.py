95502a549341c6d19cb272c5cd7b451455d1a615
WolfiD007 <dama.0@mail.ru>
2021-09-10 15:57:13 +0600

    Update Salom_Bot.py

--- Salom_Bot.py
+++ Salom_Bot.py
@@ -37,12 +37,12 @@ def serial_menu(message, start=False):
     markup.add(telebot.types.InlineKeyboardButton(text='Shaharlik Qichloqi', callback_data="shaharlik_qichilogi"))
     serialar = bot.send_message(message.chat.id, 'Serialar',
                      reply_markup=markup)
-    if start == False:
+    """if start == False:
         #bot.edit_message_reply_markup(message.chat.id, message_id=message.message_id - 1, reply_markup='')
         #bot.edit_message_text("test", chat_id=message.chat.id, message_id=to_delete.message_id)
         bot.delete_message(message.chat.id, to_delete.message_id)
         bot.delete_message(message.chat.id, to_delete_ser.message_id)
-        bot.delete_message(message.chat.id, message.message_id)
+        bot.delete_message(message.chat.id, message.message_id)"""
 
 
 
