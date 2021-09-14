b2b00823fdc10321793351d4566cbe7b579828b8
WolfiD007 <dama.0@mail.ru>
2021-09-14 14:00:32 +0600

    Update Salom_Bot.py

--- Salom_Bot.py
+++ Salom_Bot.py
@@ -28,7 +28,7 @@ def read_creds():
         f.readline()
         f.readline()
         fdbname = f.readline().strip()
-"""
+
 def serial_menu(message, start=False):
 
     global serialar, text
@@ -52,14 +52,15 @@ def serial_menu(message, start=False):
 
     serialar = bot.send_message(message.chat.id, 'Serialar',
                                 reply_markup=markup)
-"""
+
+
 
 @salom_bot.route("/" + token + "/", methods=["POST"])
 def receive_update():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     print("Message")
     return "ok", 200
-"""
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
 
@@ -187,7 +188,7 @@ def send_text(message):
                         btn = telebot.types.InlineKeyboardButton(row[0], callback_data=item['name'])
                         markup.row(btn)
             serialar = bot.send_message(message.chat.id, "Sevimlilaringiz", reply_markup=markup)
-            
+
     bot.delete_message(message.chat.id, message.message_id)
 
 
@@ -278,4 +279,3 @@ def query_handler(call):
     to_delete_ser = bot.send_message(call.message.chat.id, Strings.series_chose, reply_markup=start_markup)
     #bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 
-"""
