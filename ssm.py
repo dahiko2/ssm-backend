8c85313691a533d86d15106f805643cccdc7511c
Oleg Simonov <olegsimonoff@mail.ru>
2021-09-27 14:55:10 +0600

    fix for kassa24 description

--- ssm.py
+++ ssm.py
@@ -1028,11 +1028,8 @@ def kassa24_send_query(inp):
     callback_url = 'https://maksimsalnikov.pythonanywhere.com/ssm/kassa24_callback'
     kassa_request_url = "https://ecommerce.pult24.kz/payment/create"
     description_str = "Заказ #"+str(inp['order_number'])+"\nСодержание:\n"
-    if isinstance(inp['basket'], list):
-        for item in inp['basket']:
-            description_str += str(item['name'])+"\n"
-    else:
-        print(inp['basket'])
+    for item in inp['basket']:
+        description_str += str(item['name'])+"\n"
     payload = {
         "orderId": str(inp['order_number']),
         "merchantId": fkassa_login,
