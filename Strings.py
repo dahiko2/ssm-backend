f1f1adfcf989d85fa2d504e7067b6a023d2a1f84
WolfiD007 <dama.0@mail.ru>
2021-10-26 12:39:55 +0600

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,24 @@
+qichchu_qudrat = "Qichchu Qudrat\n" \
+                 "\n" \
+                 "Serial haqida: \n" \
+                 "Moskvada 5 yil yashagan Qudrat, qishlog'iga qaytib keladi va bir qizga oshiq bo'ladi, u unga uylanish va Moskvaga olib ketishni istaydi, lekin u Qudratni unga munosib er ko'rmaydi va otasi u yerga uni hech qachon qo'yib yubormaydi."
+
+shaharlik_qichloqi = "Shaharlik Qichloqi\n" \
+                     "\n" \
+                     "Serial haqida:\n" \
+                     "Zarnigor juda taltayib ketib otasiga ko'p muomolar tug'diradi va otasi uni tarbiyalashi uchun qishloqga ya'ni qattiq qo'l ammasini yoniga yuboradi 🚕🏔🏡.\n" \
+                     "Shaharga qaytishning yagona yo'li - otasi aytgan yigitga erga tegish 👰🏻‍♀, lekin Zara uni sevmaydi 😡.  U qishloqda qolib hech kimga aytmasdan, o'qishga magistraturaga topshiradi👩🏻‍🎓"
+
+series_chose = "Seriyalarni tanlang"
+
+maktab_desc = "Maktab\n"\
+                 "\n"\
+                 "Serial haqida: MAKTAB.\n" \
+                 "O'quvchilarining hayoti qiziqarli voqealar! Ishkal,sevgi,do'stlik  shu va shunga o'xshash qissalarni har bir maktab o'quvchisi boshidan o'tqazgan!\n" \
+                 "\n" \
+                 "Hullas kalom biz sizlar uchun ajoyib hayotiy serial su'ratga oldik. Tomosha qilmasangiz kundalikga 2 tushadi aytib qo'ydim lekin;)\n" \
+                 "\n" \
+                 "AJOYIB JAMOA BILAN AJOYIB SERIAL"
+
+start = "Assalomu alaykum!\n" \
+        "Serialni tanlang, qaysi birini tomosha qilishni istaysiz? Yoki botga serialni nomini yozing."
